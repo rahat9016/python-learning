2df26e7ab926d3973bbd7b4367bbe8eebd7b3dfe
rahat9016 <minhajurrohoman9016@gmail.com>
2024-01-08 23:02:44 +0600

    input file

---
+++ first_module/first_file.py
@@ -0,0 +1,21 @@
+"""
+    # This way i have learned triple quotation
+    # And i have learned single quotation
+"""
+
+# First way i have print hello world by using print function 
+print("Hello world...");
+
+# Second way we have declared variable using ___ (underscore) and print it.
+
+first__name = "Minhahajur"
+last_name = "Rohoman"
+
+print(first__name, last_name);
+
+# Third way i have declared boolean data type 
+
+is__subscribed = True;
+is__not__subscribed = False;
+
+print(is__subscribed, is__not__subscribed);
