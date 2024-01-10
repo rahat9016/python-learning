0f95a98709cb47b10fa8088e4796d673e11bd530
rahat9016 <minhajurrohoman9016@gmail.com>
2024-01-10 23:43:17 +0600

    if elif else condition

---
+++ first_module/if_elif_else.py
@@ -0,0 +1,18 @@
+isHot = False;
+isCold = False;
+
+if isHot:
+    print("It's hot")
+elif isCold:
+    print("It's cold")
+else:
+    print("No hot, no cold")
+
+price = 1000000
+has_good_credit= False;
+
+if has_good_credit:
+    down_payment = 0.1 * price;
+else:
+    down_payment = 0.2 * price;
+print(f"down payment : {down_payment}")
