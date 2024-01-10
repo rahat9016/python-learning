92d7590c946e9b7328a2d176fc9d6e05abf699c2
rahat9016 <minhajurrohoman9016@gmail.com>
2024-01-10 22:22:04 +0600

    if else condition

---
+++ first_module/if_else_condition.py
@@ -0,0 +1,8 @@
+is_hot = False;
+
+if is_hot:
+    print("Today is hot day");
+    print("I want drink water");
+else:
+    print("No! today is not hot day");
+    print("It's your bad day!");
---
+++ first_module/math_func.py
@@ -0,0 +1,11 @@
+import math
+
+# round function
+print(round(2.56))
+
+# make absolute value by using abs function
+print(abs(-2.5));
+
+# ceil using from math module
+print(math.ceil(2.8))
+print(int(math.pow(2, 2)));
