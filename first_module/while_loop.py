c1c3b4da5f01dfcee79c316966a502f581333a1b
rahat9016 <minhajurrohoman9016@gmail.com>
2024-01-12 21:16:01 +0600

    password currect programe

---
+++ first_module/while_loop.py
@@ -0,0 +1,16 @@
+password = 6;
+
+program_running = True;
+password_limit = 3;
+count = 0
+while program_running:
+    count += 1
+    user_pass = int(input("Enter you password:- "))
+    if(user_pass == password):
+        print("Signing successfully done!")
+        break
+    elif count > 3:
+        print(f"Unauthorized user. you gave {count} time's password")
+        break
+    else:
+        print("Password is wrong!")
